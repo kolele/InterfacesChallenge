7f1146d5435d307c548a7841853137e4bdaa42c9
Slobodan Kosanovic <slobodan.kosanovic@gmail.com>
2018-07-02 16:15:05 +0200

    a

--- src/com/kole/Player.java
+++ src/com/kole/Player.java
@@ -1,8 +1,10 @@
 package com.kole;
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by dev on 30/09/15.
+ */
 public class Player implements ISaveable {
     private String name;
     private int hitPoints;
@@ -59,23 +61,38 @@ public class Player implements ISaveable {
     }
 
     @Override
-    public ArrayList<String> write() {
+    public List<String> write() {
         List<String> values = new ArrayList<String>();
         values.add(0, this.name);
         values.add(1, "" + this.hitPoints);
         values.add(2, "" + this.strength);
         values.add(3, this.weapon);
 
-        return null;
+        return values;
     }
 
     @Override
     public void read(List<String> savedValues) {
-        if (savedValues!=null && savedValues.size()>0){
+        if(savedValues != null && savedValues.size() >0) {
             this.name = savedValues.get(0);
             this.hitPoints = Integer.parseInt(savedValues.get(1));
             this.strength = Integer.parseInt(savedValues.get(2));
             this.weapon = savedValues.get(3);
         }
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
